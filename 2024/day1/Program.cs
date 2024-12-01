488a4e244ecb348d63537cc3f0c83dd204a84746
Tanous Raad <raad@et.esiea.fr>
2024-12-01 20:10:13 +0100

    Add day 1 solution

---
+++ 2024/day1/Program.cs
@@ -0,0 +1,39 @@
+﻿// part 1
+
+string fileContent = File.ReadAllText("input");
+string[] lines = fileContent.Split("\n");
+
+List<int> leftCol = lines.Select(x => int.Parse(x.Split("   ")[0])).ToList();
+List<int> rightCol = lines.Select(x => int.Parse(x.Split("   ")[1])).ToList();
+
+leftCol.Sort();
+rightCol.Sort();
+
+int result = 0;
+
+for (int i = 0; i < leftCol.Count; i++)
+{
+    int higherValue = (leftCol[i] > rightCol[i]) ? leftCol[i] : rightCol[i];
+    int lowerValue = (leftCol[i] < rightCol[i]) ? leftCol[i] : rightCol[i];
+
+    result += higherValue - lowerValue;
+}
+
+Console.WriteLine(result);
+
+// part 2
+
+leftCol = lines.Select(x => int.Parse(x.Split("   ")[0])).ToList();
+rightCol = lines.Select(x => int.Parse(x.Split("   ")[1])).ToList();
+
+result = 0;
+
+for (int i = 0; i < leftCol.Count; i++)
+{
+    int leftColValue = leftCol[i];
+    int leftColValueCountInRightCol = rightCol.Count(x => x == leftColValue);
+
+    result += leftColValue * leftColValueCountInRightCol;
+}
+
+Console.WriteLine(result);
