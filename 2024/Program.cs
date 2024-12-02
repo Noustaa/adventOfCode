6409ef4f70358de8aa562416072cd2b7720373b2
Tanous Raad <raad@et.esiea.fr>
2024-12-02 20:38:49 +0100

    Update project structure

---
+++ 2024/Program.cs
@@ -0,0 +1,4 @@
+﻿using CurrentDay = _2024.Day1.Day1;
+
+CurrentDay.SolvePart1();
+CurrentDay.SolvePart2();
---
+++ 2024/day1/Day1.cs
@@ -0,0 +1,50 @@
+﻿namespace _2024.Day1
+{
+    public static class Day1
+    {
+        public static void SolvePart1()
+        {
+            string fileContent = File.ReadAllText("input");
+            string[] lines = fileContent.Split("\n");
+
+            List<int> leftCol = lines.Select(x => int.Parse(x.Split("   ")[0])).ToList();
+            List<int> rightCol = lines.Select(x => int.Parse(x.Split("   ")[1])).ToList();
+
+            leftCol.Sort();
+            rightCol.Sort();
+
+            int result = 0;
+
+            for (int i = 0; i < leftCol.Count; i++)
+            {
+                int higherValue = (leftCol[i] > rightCol[i]) ? leftCol[i] : rightCol[i];
+                int lowerValue = (leftCol[i] < rightCol[i]) ? leftCol[i] : rightCol[i];
+
+                result += higherValue - lowerValue;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public static void SolvePart2()
+        {
+            string fileContent = File.ReadAllText("input");
+            string[] lines = fileContent.Split("\n");
+
+            List<int> leftCol = lines.Select(x => int.Parse(x.Split("   ")[0])).ToList();
+            List<int> rightCol = lines.Select(x => int.Parse(x.Split("   ")[1])).ToList();
+
+            int result = 0;
+
+            for (int i = 0; i < leftCol.Count; i++)
+            {
+                int leftColValue = leftCol[i];
+                int leftColValueCountInRightCol = rightCol.Count(x => x == leftColValue);
+
+                result += leftColValue * leftColValueCountInRightCol;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
